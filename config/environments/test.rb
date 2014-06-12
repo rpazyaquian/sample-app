cfc9210e2d553441fa2896813f0eba59f6163e2b
Rebecca Paz <rebecca@kyn.is>
2014-06-12 11:53:01 -0400

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Speed up FG tests.
+  ActiveModel::SecurePassword.min_cost = true
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name	"Rebecca Paz"
+		email	"rebecca@user.mac"
+		password	"foobar"
+		password_confirmation	"foobar"
+	end
+end
